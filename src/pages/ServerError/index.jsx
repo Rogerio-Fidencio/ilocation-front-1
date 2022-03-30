cedc50435973175a70cdc7b51cdc1f83165bb57c
ananuness <beatriznunez601@gmail.com>
2022-03-30 16:56:08 -0300

    feat: criação de tela de server internal error

--- src/pages/ServerError/index.jsx
+++ src/pages/ServerError/index.jsx
@@ -1,5 +1,22 @@
+import './servererror.css';
+
 function ServerError() {
-  
+  return (
+    <div className="container-error">
+      <div className="error-content">
+        {/* <img className="content-img" src={errorIcon} alt="erro 500: problema interno do servidor" /> */}
+
+        <main className="content-main">
+          <h1 className="main-h1">Falha nossa!</h1>
+          <p className="main-description">
+            <b>Parece que estamos com problemas &#128549;</b>
+            <br />
+            Tente mais tarde... <br /> Enquanto isso, tome um cafezinho &#9749;
+          </p>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default ServerError;
