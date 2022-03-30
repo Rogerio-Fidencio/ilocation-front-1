6a0fe0e6a682551ddb7384a11924a19f7041e2b3
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-03-29 21:03:49 -0300

    fix fetch geolocation

--- src/pages/Tracking/index.jsx
+++ src/pages/Tracking/index.jsx
@@ -3,8 +3,9 @@ import 'leaflet/dist/leaflet.css';
 import { MapContainer,Marker,TileLayer,Popup} from 'react-leaflet'
 import "./tracking.css"
 import useGetLocation from '../../hooks/useGetLocation'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from '../../Components/Header';
+import useAuth from '../../hooks/useAuth';
 
 export default function Tracking() {
   const [ change, setChange ] = useState(true);
@@ -14,30 +15,35 @@ export default function Tracking() {
     return <h1>Obtendo localização ...</h1>
   }
 
+  
   function handleChange(){
-    while(change){
-      handleGeolocation()
-    }
+
+    handleGeolocation()
+    // while(change){
+    // }
       setChange(!change)
       // navigate('/orders', { replace: true });
   }
 
   const handleGeolocation = async(event) => {
-    event.preventDefault();
 
     //** validação básica de usuário **
-    
+
+
     const userCoords = {
-      latitude: coords[0],
+      timestamp: coords[2],
       longitude: coords[1],
-      timeStamp: coords[2]
+      latitude: coords[0]
     }; //para mandar pro back
 
+    console.log(userCoords)
+
     try {
-      const request = await fetch('endpoint de geolocation', {
-        method: 'POST',
+      const request = await fetch('http://localhost:8080/api/v1/geolocation', {
+        method: 'PUT',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJyb2dlcmlvQGVtYWlsLmNvbSIsImlzcyI6IkFkbWluIiwiZXhwIjoxNjQ4NjAxNzcyLCJ1c2VyIjoie1wiaWRcIjoxMyxcIm5hbWVcIjpcIlJvZ2VyaW9cIixcInBob25lXCI6XCI5OTg4NjY1NTQ0M1wifSJ9.B5zJ44RBEqV3l7fYlQCUH6UMEThDGJoAckYkI2Cz4eE"
         },
         body: JSON.stringify(userCoords)
       })
@@ -45,7 +51,9 @@ export default function Tracking() {
         .then((userCoords) => {
           console.log("Success:", userCoords);
         })
-      // const response = await request.json();
+      const response = await request.json();
+
+      console.log(response)
 
       //**tratamento do response **
 
@@ -100,7 +108,7 @@ export default function Tracking() {
         {/* <button type="submit" className="btn btn-primary btn-verde " onClick={handleChange()}>Concluir</button>
         <button type="submit" className="btn btn-primary " onClick={handleChange()}>Cancelar</button> */}
         <button type="submit" className="btn btn-primary btn-verde "><a href="./pedidos.html">Concluir</a></button>
-        <button type="submit" className="btn btn-primary " onClick={() => handleChange()}><a href="./pedidos.html">Cancelar</a></button>
+        <button type="submit" className="btn btn-primary " onClick={(event) => handleChange()}>Cancelar</button>
       </div>
     </>
   )
